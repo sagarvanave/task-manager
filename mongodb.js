df5e1d9ef93d91cd6fd1b67d0ad959eb173a3639
sagarvanave <ssv1437@gmail.com>
2021-05-31 00:39:16 +0530

    Fetch task from ID and fetch Incompleted tasks

--- mongodb.js
+++ mongodb.js
@@ -1,20 +1,11 @@
 //Basic CRUD check
 
-// const mongodb = require('mongodb')
-// const MongoClient = mongodb.MongoClient
-// const ObjectID = mongodb.ObjectID
-
 //Object destructing
 
 const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
-
-// const id=new ObjectID()
-// console.log(id.id.length);
-
-// console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client) => {
     if (error) {
@@ -24,64 +15,18 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 
     const db = client.db(databaseName)
 
-    // Insert one record into database
 
-    // db.collection('users').insertOne({
-    //     name:"sagar",
-    //     age:24
-    // },(error,result) => {
-    //     if(error){
-    //         return console.log("Unable to insert user")
-    //     }
-    //     console.log(result.ops)
-    // })
-
-    // Insert multiple records into the database
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name:"Rahul",
-    //         age:25
-    //     },
-    //     {
-    //         name:"Pratik",
-    //         age:26
-    //     }
-    //     ],(error,result)=>{
-    //         if(error){
-    //             return console.log("Unable to insert records")
-    //         }
-    //         console.log(result.ops)
-    //     })
-
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description:"Read a book",
-    //         completed:false
-    //     },{
-    //         description:"Charge the mobile",
-    //         completed:true
-    //     },{
-    //         description:"Watch a movie",
-    //         completed:true
-    //     }
-    // ],(error,result)=>{
-    //     if(error){
-    //         return console.log("Unable to insert records")
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-
-    //Fetch Data from database
-
-    db.collection('users').findOne({ _id:new ObjectID('60b36f2db8e7ef1d670413f9')},(error,user)=>{
+    db.collection('tasks').findOne({ _id:new ObjectID('60b37561bfe06720631bf105')},(error,task)=>{
         if(error){
-            return console.log("Unable to fetch")
+            return console.log('Unable to fetch')
         }
-        console.log(user)
+        console.log(task.description)
     })
 
+    db.collection('tasks').findOne({ completed:false},(error,task)=>{
+        if(error){
+            return console.log('Unable to fetch')
+        }
+        console.log(task.description)
+    })
 })
