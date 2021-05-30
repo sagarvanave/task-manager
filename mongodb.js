a41a838e8b64002b0e03974a8e31bbe803265370
sagarvanave <ssv1437@gmail.com>
2021-05-30 16:56:22 +0530

    how to insert a multiple records in the database

--- mongodb.js
+++ mongodb.js
@@ -14,9 +14,53 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name:"sagar",
-        age:24
-    })
+    // Insert one record into database
 
+    // db.collection('users').insertOne({
+    //     name:"sagar",
+    //     age:24
+    // },(error,result) => {
+    //     if(error){
+    //         return console.log("Unable to insert user")
+    //     }
+    //     console.log(result.ops)
+    // })
+
+    // Insert multiple records into the database
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name:"Rahul",
+    //         age:25
+    //     },
+    //     {
+    //         name:"Pratik",
+    //         age:26
+    //     }
+    //     ],(error,result)=>{
+    //         if(error){
+    //             return console.log("Unable to insert records")
+    //         }
+    //         console.log(result.ops)
+    //     })
+
+
+    db.collection('tasks').insertMany([
+        {
+            description:"Read a book",
+            completed:false
+        },{
+            description:"Charge the mobile",
+            completed:true
+        },{
+            description:"Watch a movie",
+            completed:true
+        }
+    ],(error,result)=>{
+        if(error){
+            return console.log("Unable to insert records")
+        }
+        
+        console.log(result.ops)
+    })
 })
